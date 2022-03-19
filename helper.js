2cc4a578307d6e96407c5caa263129cf13323c5a
Parag Bharadva <parag.b@simformsolutions.com>
2022-03-19 11:28:17 +0530

    Date Format fixed

--- helper.js
+++ helper.js
@@ -1,20 +1,29 @@
 import moment from "moment";
 //This is a helper function for the hbs for setting the ui of past messages
 export default {
-    messageShow: function(user, message, options){
+    messageShow: function (user, message, options) {
         let ele = ""
         message.forEach(element => {
-            if(element.from.username===user){
-                ele+=`<div class="outgoing">
+            let date = new Date(element.time)
+            console.log(date.getDate());
+            let today = new Date()
+            console.log(today.getDate())
+            let momentVar = date.getDate() == today.getDate() ?
+                moment(element.time).format("h:mm a") :
+                moment(element.time).format("h:mm a DD-MM-YY")
+            console.log(momentVar);
+            if (element.from.username === user) {
+
+                ele += `<div class="outgoing">
                 <p>${element.context}</p><span class="text-muted">
-                <small>${moment(element.time).format("h:mm a")}</small>
+                <small>${momentVar}</small>
                 </span>
                 </div>`
-            }else{
-                ele+=`<div class="incoming">
+            } else {
+                ele += `<div class="incoming">
                 <div class="msgBy">${element.from.username}</div>
                 <p>${element.context}</p><span class="text-muted">
-                <small>${moment(element.time).format("h:mm a")}</small>
+                <small>${momentVar}</small>
                 </span>
                 </div>`
             }
